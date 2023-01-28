0c4d386fc77edb2bef8a0edae44532926c8e5c4a
Magnus Gunnarsson <magnus@gunnarssons.eu>
2023-01-28 13:22:31 +0100

    finished some more exercises!

--- exercises/variables/variables3.rs
+++ exercises/variables/variables3.rs
@@ -2,12 +2,8 @@
 // Execute `rustlings hint variables3` or use the `hint` watch subcommand for a hint.
 
 fn main() {
-<<<<<<< HEAD
     let mut x:u32 = 3;
     println!("Number {}", x);
     x = 5; // don't change this line
-=======
-    let x: i32;
->>>>>>> ce7ffcd8e917b32318c99561762be75c9b1f0870
     println!("Number {}", x);
 }
--- exercises/variables/variables4.rs
+++ exercises/variables/variables4.rs
@@ -2,12 +2,6 @@
 // Execute `rustlings hint variables4` or use the `hint` watch subcommand for a hint.
 
 fn main() {
-<<<<<<< HEAD
     let x: i32=0;
-=======
-    let x = 3;
-    println!("Number {}", x);
-    x = 5; // don't change this line
->>>>>>> ce7ffcd8e917b32318c99561762be75c9b1f0870
     println!("Number {}", x);
 }
